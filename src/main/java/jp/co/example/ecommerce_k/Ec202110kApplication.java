d28518ea1cac6d5934896463ca7b45cedf8fcbfe
kanomatahiroyuki <hiroyuki.kanomata.rks@gmail.com>
2021-11-25 10:46:33 +0900

    first commit

---
+++ src/main/java/jp/co/example/ecommerce_k/Ec202110kApplication.java
@@ -0,0 +1,13 @@
+package jp.co.example.ecommerce_k;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec202110kApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec202110kApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/co/example/ecommerce_k/Ec202110kApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.example.ecommerce_k;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec202110kApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
