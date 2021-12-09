9947474fb91f9bce7443476cf50fb4a6ad32f4fa
hiroyuki.kanomata <>
2021-12-09 14:33:06 +0900

    ユーザ関連テーブルのDDLとDMLをプロジェクトに追加

---
+++ sql/create_table.sql
@@ -0,0 +1,27 @@
+/* ユーザーマスタテーブル */
+CREATE TABLE IF NOT EXISTS m_user (
+  user_id VARCHAR(50) PRIMARY KEY,
+  password VARCHAR(255),
+  pass_update_date TIMESTAMP,
+  login_miss_times INT,
+  unlock boolean,
+  tenant_id VARCHAR(50),
+  user_name VARCHAR(50),
+  mail_address VARCHAR(50),
+  enabled BOOLEAN,
+  user_due_date TIMESTAMP
+);
+
+/* 権限マスタテーブル */
+CREATE TABLE IF NOT EXISTS m_role (
+  role_id VARCHAR(50) PRIMARY KEY,
+  role_name VARCHAR(100)
+);
+
+/* ユーザー権限テーブル */
+/* ユーザーと権限を紐付けるテーブル */
+CREATE TABLE IF NOT EXISTS t_user_role (
+  id INT PRIMARY KEY,
+  user_id VARCHAR(50),
+  role_id VARCHAR(50)
+);
